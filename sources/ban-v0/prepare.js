a19d6b873ff6418cb6d6a3cbb33e1f786bc945a7
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-06-04 01:17:04 +0200

    Clean nomVoie from BAN

--- sources/ban-v0/prepare.js
+++ sources/ban-v0/prepare.js
@@ -1,12 +1,29 @@
+const {memoize, deburr} = require('lodash')
 const recomputeCodesVoies = require('../../lib/processing/recompute-codes-voies')
 const removeStacked = require('../../lib/processing/remove-stacked')
 const updateCommunes = require('../../lib/processing/update-communes')
 
 async function prepareData(adressesCommune) {
+  const cleanNomVoie = memoize(nomVoie => {
+    const cleanedNomVoie = nomVoie.split('/')[0].split('(')[0]
+    if (deburr(cleanedNomVoie).match(/^[a-z]/i)) {
+      return cleanedNomVoie
+    }
+
+    console.log(`Nom de voie non valide : ${nomVoie}`)
+  })
+
   const filteredAdressesCommune = adressesCommune.filter(adresse => {
     if (!adresse.numero || !adresse.nomVoie) {
       return false
     }
+
+    const cleanedNomVoie = cleanNomVoie(adresse.nomVoie)
+    if (!cleanedNomVoie) {
+      return false
+    }
+
+    adresse.nomVoie = cleanedNomVoie
 
     // Suppression des pseudo-numéros, approche grossière pour commencer.
     // Il existe des cas de 5000 légitimes, notamment pour la numérotation métrique et lorsque la voie comporte des 3000 ou 4000
