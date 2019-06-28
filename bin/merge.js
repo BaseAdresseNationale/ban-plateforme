896f64a4bc596177a40dedd48a7b60911f19910e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-06-28 23:02:06 +0200

    Multi-sources input

--- bin/merge.js
+++ bin/merge.js
@@ -7,16 +7,21 @@ const {runInParallel} = require('../lib/cli/parallel')
 async function main() {
   const communes = getCommunes()
 
-  if (!process.env.SOURCES) {
-    throw new Error('La liste des sources à prendre en compte doit être définie (SOURCES)')
+  if (!process.env.MERGE_INPUT_SOURCES) {
+    throw new Error('La liste des fichiers sources à prendre en compte doit être définie (MERGE_INPUT_SOURCES)')
   }
 
-  const sources = process.env.SOURCES.split(',')
+  if (!process.env.MERGE_FILTER_SOURCES) {
+    throw new Error('La liste des sources à prendre en compte doit être définie (MERGE_FILTER_SOURCES)')
+  }
+
+  const inputSources = process.env.MERGE_INPUT_SOURCES.split(',')
+  const filterSources = process.env.MERGE_FILTER_SOURCES.split(',')
   const licences = process.env.LICENCES ? process.env.LICENCES.split(',') : undefined
 
   const communesStats = await runInParallel(
     require.resolve('../lib/merge'),
-    communes.map(codeCommune => ({codeCommune, sources, licences})),
+    communes.map(codeCommune => ({codeCommune, inputSources, filterSources, licences})),
     {maxWorkerMemory: 3072}
   )
 
