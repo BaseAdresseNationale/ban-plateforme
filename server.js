8d3280e6bb93d41016ba2cc1baa8b43e1c5ec189
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-12-02 00:05:12 +0100

    Add initial API

---
+++ server.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongo = require('./lib/util/mongo')
+
+async function main() {
+  await mongo.connect()
+
+  const app = express()
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+  }
+
+  app.use(cors({origin: true}))
+
+  app.get('/ping', (req, res) => {
+    res.send('pong')
+  })
+
+  app.use('/', require('./lib/api/routes'))
+
+  const port = process.env.PORT || 5000
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+  })
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
