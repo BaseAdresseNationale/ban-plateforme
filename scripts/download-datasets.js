4068a15c78999fe2a8404129627b02932c38499a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2022-03-06 23:45:38 +0100

    Improve download-datasets script

--- scripts/download-datasets.js
+++ scripts/download-datasets.js
@@ -1,17 +1,23 @@
 #!/usr/bin/env node
 const path = require('path')
+const {createWriteStream} = require('fs')
+const {pipeline} = require('stream/promises')
+const {mkdirp} = require('fs-extra')
 const got = require('got')
-const {outputFile} = require('fs-extra')
 
 const dataDir = path.join(__dirname, '..', 'data')
 
 async function downloadFile(url, fileName) {
-  const response = await got(url, {responseType: 'buffer'})
-  await outputFile(path.join(dataDir, fileName), response.body)
+  await pipeline(
+    got.stream(url, {responseType: 'buffer'}),
+    createWriteStream(path.join(dataDir, fileName))
+  )
   console.log(` * ${fileName} téléchargé`)
 }
 
 async function main() {
+  await mkdirp(dataDir)
+
   await downloadFile(
     'https://www.data.gouv.fr/fr/datasets/r/9a4a5188-8142-4c9d-b3e6-f54594848509',
     'communes-locaux-adresses.json'
