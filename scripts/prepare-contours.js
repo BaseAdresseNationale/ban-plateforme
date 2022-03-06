f0286fce443e7da91223296a55857485317903d9
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2022-03-06 23:44:04 +0100

    Move communes-50m.sqlite file to data dir

--- scripts/prepare-contours.js
+++ scripts/prepare-contours.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+const path = require('path')
+const {mkdirp} = require('fs-extra')
 const {prepareContours} = require('../lib/util/contours')
 
 async function main() {
+  await mkdirp(path.join(__dirname, '..', 'data'))
   await prepareContours()
   process.exit(0)
 }
