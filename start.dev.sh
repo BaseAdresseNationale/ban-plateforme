67b6aade5f08aa62ced20d7cb4fb869ebafa49b4
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2023-02-09 18:38:34 +0100

    Added container implementation for local deploy

---
+++ start.dev.sh
@@ -0,0 +1,38 @@
+
+
+echo "FORCE_DOWNLOAD_CONTOUR : $FORCE_DOWNLOAD_CONTOUR"
+echo "FORCE_DOWNLOAD_DATASETS : $FORCE_DOWNLOAD_DATASETS"
+
+if [[ -f 'data/communes-50m.sqlite' ]]; then
+    echo "contours already exists."
+    if [[ "$FORCE_DOWNLOAD_CONTOUR" = "true" ]]; then
+        echo "forcing contours download..."
+        npm run prepare-contours
+    fi
+else
+    echo "contours does not exist."
+    echo "downloading contours"
+    npm run prepare-contours
+fi
+
+if [[ -f 'data/communes-locaux-adresses.json' && -f 'data/fantoir.sqlite' && -f 'data/gazetteer.sqlite' ]]; then
+    echo "data sets already exist."
+    if [[ "$FORCE_DOWNLOAD_DATASETS" = "true" ]]; then
+        echo "forcing datasets download..."
+        npm run download-datasets
+    fi
+else
+    echo "data sets do not exist."
+    echo "downloading data sets"
+    npm run download-datasets
+fi
+
+npm run import:ign-api-gestion
+npm run import:cadastre
+npm run import:ftth
+
+# npm run apply-batch-certification
+# npm run compose
+# npm run dist
+
+pm2-runtime process.dev.yml
