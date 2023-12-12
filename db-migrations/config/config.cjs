c5d96067b8ce3434e6e48043bf81fa07bddfaa71
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2023-12-12 17:11:02 +0100

    Added postgres schema configuration

--- db-migrations/config/config.cjs
+++ db-migrations/config/config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   development: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DBNAME,
+    database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_URL,
     port: 5432,
     dialect: 'postgres',
@@ -14,7 +14,7 @@ module.exports = {
   production: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DBNAME,
+    database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_URL,
     port: 5432,
     dialect: 'postgres',
