9a8691149223582d66bfed74d25b648a21c448d3
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2024-07-08 15:52:48 +0200

    Revert "add use case function to select calculate address postal code"
    This reverts commit 0d6b6812a542cedec29edf4406a859dd39e17f17.

--- db-migrations/migrations/20240625132013-init-postal-code-view.cjs
+++ db-migrations/migrations/20240625132013-init-postal-code-view.cjs
@@ -7,64 +7,70 @@ module.exports = {
   async up(queryInterface) {
     try {
       // Create the address_view
-
       await queryInterface.sequelize.query(`
-      CREATE VIEW ban."address_view_cp" AS
-      WITH postal_codes_array AS (
-        SELECT
-        a.id,
-        a."mainCommonToponymID",
-        a."secondaryCommonToponymIDs",
-        a.labels,
-        a.suffix,
-        a.positions,
-        a.certified,
-        a.bbox,
-            array_length(d."postalCodes", 1) AS array_length,
-            d."postalCodes" AS ps,
-            b.meta->'insee'->>'cog' AS insee_com,
-        d."libelleAcheminement"
-        FROM 
-            ban.address_view AS a
-        LEFT JOIN 
-            ban.district AS b
-            ON a."districtID" = b.id
-        LEFT JOIN 
-            external.datanova AS d
-            ON b.meta->'insee'->>'cog' = d."inseeCom"
-    )
-    SELECT
-        pca.id,
-        pca."mainCommonToponymID",
-        pca."secondaryCommonToponymIDs",
-        pca.insee_com,
-        CASE
-            WHEN pca.array_length = 1 THEN pca.ps[1]
-            WHEN pca.array_length > 1
-            THEN (
-                SELECT c."postalCode"
-                FROM external.postal_area AS c
-                WHERE pca.insee_com = c."inseeCom"
-                ORDER BY ST_Area(ST_Intersection(ST_Transform(pca.bbox, 2154), ST_Transform(c.geometry, 2154))) desc
-                LIMIT 1
-            )
-            ELSE NULL
-        END AS postal_code,
-        CASE
-            WHEN pca.array_length = 1 THEN 'DATANOVA'
-            WHEN pca.array_length > 1 
-            THEN 'CONTOURS_CP'
-            ELSE 'DGFIP' 
-        END AS source_cp,
-        pca."libelleAcheminement",
-        pca.labels,
-        pca.suffix,
-        pca.positions,
-        pca.certified,
-        pca.bbox
-    FROM 
-        postal_codes_array AS pca
-        ORDER BY pca.id ASC
+        CREATE VIEW ban."address_view_cp" AS
+        WITH postal_codes_array AS (
+          SELECT
+          a.id,
+          a."mainCommonToponymID",
+          a."secondaryCommonToponymIDs",
+          a.labels,
+          a.suffix,
+          a.positions,
+          a.certified,
+          a.bbox,
+              array_length(d."postalCodes", 1) AS array_length,
+              d."postalCodes" AS ps,
+              b.meta->'insee'->>'cog' AS insee_com,
+          d."libelleAcheminement"
+          FROM 
+              ban.address_view AS a
+          LEFT JOIN 
+              ban.district AS b
+              ON a."districtID" = b.id
+          LEFT JOIN 
+              external.datanova AS d
+              ON b.meta->'insee'->>'cog' = d."inseeCom"
+      )
+      SELECT
+          pca.id,
+          pca."mainCommonToponymID",
+          pca."secondaryCommonToponymIDs",
+          pca.insee_com,
+          CASE
+              WHEN pca.array_length = 1 THEN pca.ps[1]
+              WHEN pca.array_length > 1 AND EXISTS (
+           		 SELECT 1
+            	FROM external.postal_area AS c
+            	WHERE ST_Intersects(ST_Transform(pca.bbox, 2154), ST_Transform(c.geometry, 2154)))
+			  THEN (
+                  SELECT c."postalCode"
+                  FROM external.postal_area AS c
+                  WHERE pca.insee_com = c."inseeCom"
+                  ORDER BY ST_Area(ST_Intersection(ST_Transform(pca.bbox, 2154), ST_Transform(c.geometry, 2154))) desc
+                  LIMIT 1
+              )
+              ELSE NULL
+          END AS postal_code,
+          CASE
+              WHEN pca.array_length = 1 THEN 'DATANOVA'
+              WHEN pca.array_length > 1 AND 
+			  EXISTS (
+           		 SELECT 1
+            	FROM external.postal_area AS c
+            	WHERE ST_Intersects(ST_Transform(pca.bbox, 2154), ST_Transform(c.geometry, 2154)))
+			  THEN 'CONTOURS_CP'
+              ELSE 'DGFIP' 
+          END AS source_cp,
+          pca."libelleAcheminement",
+          pca.labels,
+          pca.suffix,
+          pca.positions,
+          pca.certified,
+          pca.bbox
+      FROM 
+          postal_codes_array AS pca
+          ORDER BY pca.id ASC
       `)
       await queryInterface.sequelize.query(`GRANT SELECT ON ban."address_view_cp" TO "${POSTGRES_BAN_USER}";`)
 
@@ -104,8 +110,11 @@ module.exports = {
         pca.insee_com,
         CASE
             WHEN pca.array_length = 1 THEN pca.ps[1]
-            WHEN pca.array_length > 1 
-            THEN (
+            WHEN pca.array_length > 1 AND EXISTS (
+                SELECT 1
+                FROM external.postal_area AS c
+                WHERE ST_Intersects(ST_Transform(pca.used_bbox, 2154), ST_Transform(c.geometry, 2154))
+            ) THEN (
                 SELECT c."postalCode"
                 FROM external.postal_area AS c
           WHERE pca.insee_com = c."inseeCom" 
@@ -116,7 +125,11 @@ module.exports = {
         END AS postal_code,
         CASE
             WHEN pca.array_length = 1 THEN 'DATANOVA'
-            WHEN pca.array_length > 1 THEN 'CONTOURS_CP'
+            WHEN pca.array_length > 1 AND EXISTS (
+                SELECT 1
+                FROM external.postal_area AS c
+                WHERE ST_Intersects(ST_Transform(pca.used_bbox, 2154), ST_Transform(c.geometry, 2154))
+            ) THEN 'CONTOURS_CP'
             ELSE 'DGFIP'
         END AS source_cp,
         pca."libelleAcheminement",
