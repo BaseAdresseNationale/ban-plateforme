53de648a19737db30cc4e45280ed73e2bccf57a8
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2024-08-13 12:28:23 +0200

    change column name and use jsonb type for address

--- db-migrations/migrations/20240808162138-init-certificate-table.cjs
+++ db-migrations/migrations/20240808162138-init-certificate-table.cjs
@@ -1,87 +1,83 @@
-'use strict';
+'use strict'
 
-require('dotenv').config();
+require('dotenv').config()
 
-const { POSTGRES_BAN_USER } = process.env;
+const {POSTGRES_BAN_USER} = process.env
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     try {
       // Create ban schema if not exists
-      await queryInterface.sequelize.query('CREATE SCHEMA IF NOT EXISTS ban;');
-      
+      await queryInterface.sequelize.query('CREATE SCHEMA IF NOT EXISTS ban;')
+
       // Grant permissions to ban user on schema ban
-      await queryInterface.sequelize.query(`GRANT USAGE ON SCHEMA ban TO "${POSTGRES_BAN_USER}";`);
+      await queryInterface.sequelize.query(
+        `GRANT USAGE ON SCHEMA ban TO "${POSTGRES_BAN_USER}";`
+      )
 
       // Create Certificate Table if not exists
-      await queryInterface.createTable('certificate', {
-        id: {
-          type: Sequelize.UUID,
-          defaultValue: Sequelize.UUIDV4,
-          allowNull: false,
-          primaryKey: true,
-        },
-        address_id: {
-          type: Sequelize.UUID,
-          allowNull: false,
-          references: {
-            model: {
-              tableName: 'address',
-              schema: 'ban',
-            },
-            key: 'id',
+      await queryInterface.createTable(
+        'certificate',
+        {
+          id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
           },
-          onUpdate: 'CASCADE',
-          onDelete: 'CASCADE',
+          // eslint-disable-next-line camelcase
+          address_id: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: {
+                tableName: 'address',
+                schema: 'ban',
+              },
+              key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          },
+          // eslint-disable-next-line camelcase
+          full_address: {
+            type: Sequelize.JSONB,
+            allowNull: false,
+          },
+          parcelles: {
+            type: Sequelize.ARRAY(Sequelize.STRING),
+            allowNull: true,
+          },
+          createdAt: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW,
+          },
         },
-        number: {
-          type: Sequelize.INTEGER,
-          allowNull: true,
-        },
-        suffix: {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-        rue: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        nom_commune: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        cog: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        parcelles: {
-          type: Sequelize.ARRAY(Sequelize.STRING),
-          allowNull: true,
-        },
-        createdAt: {
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.NOW,
+        {
+          schema: 'ban',
+          timestamps: false,
+          ifNotExists: true,
         }
-      }, {
-        schema: 'ban',
-        timestamps: false,
-        ifNotExists: true,
-      });
+      )
 
       // Grant permissions to ban user on the certificate table
-      await queryInterface.sequelize.query(`GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ban.certificate TO "${POSTGRES_BAN_USER}";`);
+      await queryInterface.sequelize.query(
+        `GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ban.certificate TO "${POSTGRES_BAN_USER}";`
+      )
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   },
 
   async down(queryInterface) {
     try {
       // Drop the Certificate table
-      await queryInterface.sequelize.query('DROP TABLE IF EXISTS ban.certificate CASCADE;');
+      await queryInterface.sequelize.query(
+        'DROP TABLE IF EXISTS ban.certificate CASCADE;'
+      )
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  }
-};
+  },
+}
