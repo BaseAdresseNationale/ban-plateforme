0d9143116da486bd4db751a9fc7a87f1afcb472c
anatolehornn <anatolehornn@users.noreply.github.com>
2025-07-28 11:45:37 +0200

    add enable addressing certification. (#575)
    * add session and action tables migration.
    
    * add session and action sequelize model.
    
    * add session and action schema validation.
    
    * add session, action, district models functions.
    
    * add session, district routes
    
    * add commune models functions.
    
    * add ProConnect middleware.
    
    * update docker compose internal host gateway.
    
    * add rollback district, remove double update commune.

---
+++ db-migrations/migrations/20250624094526-init-session.cjs
@@ -0,0 +1,95 @@
+'use strict'
+
+const {POSTGRES_BAN_USER} = process.env
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    try {
+      // Grant permissions to ban user on shema ban
+      await queryInterface.sequelize.query(`GRANT USAGE ON SCHEMA ban TO "${POSTGRES_BAN_USER}";`)
+
+      // Create District Table if not exists
+      await queryInterface.createTable('session', {
+        id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+        },
+        sub: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        givenName: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        familyName: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        usualName: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        siret: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        aud: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        exp: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        iat: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        iss: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        }
+      }, {
+        schema: 'ban',
+      })
+
+      // Grant permissions to ban user
+      await queryInterface.sequelize.query(`GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ban.session TO "${POSTGRES_BAN_USER}";`)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    try {
+      await queryInterface.sequelize.query('DROP TABLE IF EXISTS ban.session CASCADE;')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
