cba98a78785017c62c3fb56d11e9d8e7ad72467a
anatole <anatole.hornn@gmail.com>
2025-10-06 15:14:14 +0200

    add process when data is a insee code.

--- apps/bal-parser/src/index.ts
+++ apps/bal-parser/src/index.ts
@@ -1,11 +1,8 @@
 import rascal, { BrokerConfig, ConnectionAttributes } from 'rascal';
-
 import { env } from '@ban/config';
-
+import { Bal } from '@ban/types/bal-types';
 import { getDistrictIDsFromDB } from './services/bal.js';
-
-
-// import { getRevisionData } from "./helpers/dump-api/index.js";
+import { getRevisionData } from "./helpers/dump-api/index.js";
 import validator from './helpers/validator.js';
 import getBalVersion from './helpers/get-bal-version.js';
 import csvBalToJsonBal from './helpers/csv-bal-to-json-bal.js';
@@ -48,28 +45,35 @@ async function main() {
     const subscription = await broker.subscribe('balUploaded');
     subscription.on('message', async (message:any, content:any, ackOrNack) => {
       try {
-        
         // Convert csv to json
-        const dataBal = content.payload;
-        const parsedRows = await csvBalToJsonBal(dataBal);
-        
-        // Detect BAL version
-        const version = getBalVersion(parsedRows);
-        
-        console.log('[bal-parser] BAL parsée avec', parsedRows.length, 'lignes');
-        
-        // Get BAL text data from dump-api
-        // const { revision, balTextData: balCsvData } = await getRevisionData(cog);
+        const data = content.payload;
 
-        // @todo: manage multiple cogs
-        const cog = parsedRows[0].commune_insee;
-        const districtIDsFromDB = await getDistrictIDsFromDB(cog);
-
-        let useBanId = false;
-        useBanId = await validator(districtIDsFromDB, parsedRows, version, { cog });
-
-        await broker.publish('balParsed', { id: content.id, meta: { useBanId }, rows: parsedRows });
-        ackOrNack();
+        // bal
+        if (typeof data === Bal){
+          const parsedRows = await csvBalToJsonBal(data);
+        
+          // Detect BAL version
+          const version = getBalVersion(parsedRows);
+          
+          console.log('[bal-parser] BAL parsée avec', parsedRows.length, 'lignes');
+          
+          
+          // @todo: manage multiple cogs
+          const cog = parsedRows[0].commune_insee;
+          const districtIDsFromDB = await getDistrictIDsFromDB(cog);
+          
+          let useBanId = false;
+          useBanId = await validator(districtIDsFromDB, parsedRows, version, { cog });
+          
+          await broker.publish('balParsed', { id: content.id, meta: { useBanId }, rows: parsedRows });
+          ackOrNack();          
+          
+        // code insee
+        } else if (typeof data === 'string') {
+          // Get BAL text data from dump-api
+          const { balTextData: balCsvData } = await getRevisionData(data);
+          await broker.publish('balParsed', { id: content.id, meta: { source: 'BAL' }, rows: balCsvData });
+        }
       } catch (err) {
         console.error('[bal-parser] Erreur:', err);
         ackOrNack(err as Error);
