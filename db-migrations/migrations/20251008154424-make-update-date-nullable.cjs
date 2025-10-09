fb1d55d1cec7676a14537709dcbaf34d0d1cd32b
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2025-10-09 10:53:27 +0200

    fix linter error

--- db-migrations/migrations/20251008154424-make-update-date-nullable.cjs
+++ db-migrations/migrations/20251008154424-make-update-date-nullable.cjs
@@ -1,4 +1,5 @@
-'use strict';
+/* eslint-disable unicorn/filename-case, semi, eol-last */
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -7,20 +8,20 @@ module.exports = {
       // Tables principales
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.common_toponym ALTER COLUMN "updateDate" DROP NOT NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.address ALTER COLUMN "updateDate" DROP NOT NULL;'
-      );
+      )
       // Tables d'historique (si elles existent)
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.common_toponym_h ALTER COLUMN "updateDate" DROP NOT NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.address_h ALTER COLUMN "updateDate" DROP NOT NULL;'
-      );
+      )
     } catch (error) {
-      console.error(error);
-      throw error;
+      console.error(error)
+      throw error
     }
   },
   async down(queryInterface) {
@@ -28,32 +29,32 @@ module.exports = {
       // 1. Remplir les NULL dans les tables principales ET d'historique
       await queryInterface.sequelize.query(
         'UPDATE ban.common_toponym SET "updateDate" = NOW() WHERE "updateDate" IS NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'UPDATE ban.address SET "updateDate" = NOW() WHERE "updateDate" IS NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'UPDATE ban.common_toponym_h SET "updateDate" = NOW() WHERE "updateDate" IS NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'UPDATE ban.address_h SET "updateDate" = NOW() WHERE "updateDate" IS NULL;'
-      );
+      )
       // 2. Remettre NOT NULL
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.common_toponym ALTER COLUMN "updateDate" SET NOT NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.address ALTER COLUMN "updateDate" SET NOT NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.common_toponym_h ALTER COLUMN "updateDate" SET NOT NULL;'
-      );
+      )
       await queryInterface.sequelize.query(
         'ALTER TABLE ban.address_h ALTER COLUMN "updateDate" SET NOT NULL;'
-      );
+      )
     } catch (error) {
-      console.error(error);
-      throw error;
+      console.error(error)
+      throw error
     }
   },
-};
+}
