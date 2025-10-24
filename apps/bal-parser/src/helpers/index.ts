c5739e9d47bd73259c67bd18bb602e92c36836ee
Nicolas KOKLA <n.kokla@koaji.fr>
2025-10-25 00:47:29 +0100

    [BAL-Parser] Implement `ID-Fix` logic and features into BAL-Parser

---
+++ apps/bal-parser/src/helpers/index.ts
@@ -0,0 +1,5 @@
+export { default as csvBalToJsonBal } from './csv-bal-to-json-bal.js';
+export { default as digestIDsFromBalAddr } from './digest-ids-from-bal-addr.js';
+export { default as digestIDsFromBalUIDs } from './digest-ids-from-bal-uids.js';
+export { default as getBalVersion } from './get-bal-version.js';
+export { default as validator } from './validator.js';
--- apps/bal-parser/src/index.ts
+++ apps/bal-parser/src/index.ts
@@ -1,17 +1,24 @@
-import rascal from 'rascal';
+import rascal, { BrokerConfig, ConnectionAttributes } from 'rascal';
 
 import { env } from '@ban/config';
 
-import parseBalForBan from './parseBalForBan.js';
+import { getDistrictIDs } from './services/bal.js';
 
-const rabbitConfig = {
+
+import validator from './helpers/validator.js';
+import getBalVersion from './helpers/get-bal-version.js';
+import csvBalToJsonBal from './helpers/csv-bal-to-json-bal.js';
+
+const AUTO_CREATE_DISTRICT = true; // Auto create district if not found
+
+const rabbitConfig: ConnectionAttributes = {
   hostname: env.RABBIT.host,
   port: Number(env.RABBIT.port),
   user: env.RABBIT.user,
   password: env.RABBIT.password,
 };
 
-const config = {
+const config: BrokerConfig = {
   vhosts: {
     '/': {
       connection: {
@@ -40,11 +47,37 @@ async function main() {
   try {
     const broker = await rascal.BrokerAsPromised.create(config);
     const subscription = await broker.subscribe('balUploaded');
-    subscription.on('message', async (message, content, ackOrNack) => {
+    subscription.on('message', async (message:any, content:any, ackOrNack) => {
       try {
-        const parsedRows = await parseBalForBan(content.payload);
+        const {type, payload} = content;
+
+        console.log(`[bal-parser] Nouveau message BAL reçu (type: ${type}) :`, typeof content !== 'string' ? JSON.stringify(content, null, 2) : content);
+
+        const dataBal = payload;
+        console.log(`[bal-parser] BAL CSV reçue via CSV message`, typeof dataBal);
+
+        // Convert csv to json
+        const parsedRows = await csvBalToJsonBal(dataBal);
+
+        // Detect BAL version
+        const version = getBalVersion(parsedRows);
+
         console.log('[bal-parser] BAL parsée avec', parsedRows.length, 'lignes');
-        await broker.publish('balParsed', { id: content.id, rows: parsedRows });
+
+        const cog = parsedRows[0].commune_insee; // TODO: manage multiple cogs
+        let districtIDs;
+        const shouldThrowError = !AUTO_CREATE_DISTRICT;
+        districtIDs = await getDistrictIDs(cog, shouldThrowError);
+
+        if (AUTO_CREATE_DISTRICT && !districtIDs) {
+          districtIDs = [...(new Set(parsedRows.map(({ id_ban_commune }) => id_ban_commune)))]
+          console.log('[bal-parser] Création automatique des districts pour les COGs trouvés dans la BAL :', districtIDs);
+        }
+
+        let useBanId = false;
+        useBanId = await validator(districtIDs || [], parsedRows, version, { cog });
+
+        await broker.publish('balParsed', { id: content.id, meta: { useBanId }, rows: parsedRows });
         ackOrNack();
       } catch (err) {
         console.error('[bal-parser] Erreur:', err);
@@ -52,7 +85,7 @@ async function main() {
       }
     });
 
-    console.log('[bal-parser] En écoute...');
+    console.log('[bal-parser] En écoute...', AUTO_CREATE_DISTRICT ? ' (Auto création des districts activée)' : '');
   } catch (err) {
     console.error('[bal-parser] Erreur:', err);
     process.exit(1);
