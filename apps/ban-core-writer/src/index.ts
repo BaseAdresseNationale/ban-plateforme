6a5a6c6c85484b74d5f55d90c2030319b58d31f9
Nicolas KOKLA <n.kokla@koaji.fr>
2025-12-09 01:55:16 +0100

    [BAN-Core-Writer] Implement temporary simple logger

--- apps/ban-core-writer/src/index.ts
+++ apps/ban-core-writer/src/index.ts
@@ -4,6 +4,8 @@ import pg from 'pg';
 
 import { env } from '@ban/config';
 
+import logger from './tools/logger.js';
+
 import { getBanObjectsFromBalRows } from './helper.js';
 
 const rabbitConfig = {
@@ -50,7 +52,7 @@ const mongoUrl =
   env.MONGO.username && env.MONGO.password
     ? `mongodb+srv://${env.MONGO.username}:${env.MONGO.password}@${env.MONGO.host}?replicaSet=replicaset&tls=true&authSource=admin&readPreference=primary`
     : `mongodb://${env.MONGO.host}:${env.MONGO.port}`;
-console.log(`[ban-writer] Mongo URL: ${mongoUrl}`);
+logger.info(`[ban-writer] Mongo URL: ${mongoUrl}`);
 const mongoDbName = env.MONGO.db;
 const pgConfig = {
   host: env.PG.host,
@@ -85,27 +87,27 @@ async function main() {
 
       if (hasAllIds) {
         // Écriture PostgreSQL simulée (à remplacer par du vrai SQL)
-        console.log(`[writer] Enregistrement PostgreSQL de ${parsed.rows.length} lignes`);
+        logger.info(`[writer] Enregistrement PostgreSQL de ${parsed.rows.length} lignes`);
       } else {
-        console.log(`[writer] Données incomplètes pour PostgreSQL, passage MongoDB`);
+        logger.info(`[writer] Données incomplètes pour PostgreSQL, passage MongoDB`);
       }
 
       // MongoDB
       const banObjects = getBanObjectsFromBalRows(parsed.rows, DEFAULT_ISO_CODE);
 
       // Delete existing documents in MongoDB
-      console.log(`[writer] Suppression des documents existants pour BAL ${parsed.id}`);
+      logger.info(`[writer] Suppression des documents existants pour BAL ${parsed.id}`);
       const parsedDistrictsIds = Object.keys(banObjects.districts);
       await Promise.all([
         mongoCollectionAddresses.deleteMany({ districtID: { $in: parsedDistrictsIds } }),
         mongoCollectionCommonToponyms.deleteMany({ districtID: { $in: parsedDistrictsIds } }),
         mongoCollectionDistricts.deleteMany({ id: { $in: parsedDistrictsIds } }),
       ]);
-      console.log(`[writer] Documents existants pour BAL ${parsed.id} contenant le(s) district(s) ${parsedDistrictsIds.join(', ')} supprimé(s)`);
+      logger.info(`[writer] Documents existants pour BAL ${parsed.id} contenant le(s) district(s) ${parsedDistrictsIds.join(', ')} supprimé(s)`);
 
       // Enregistrement MongoDB
       const currentTimestamp = new Date().toISOString();
-      console.log(`[writer] Enregistrement MongoDB de BAL ${parsed.id} avec ${Object.keys(banObjects.districts).length} districts, ${Object.keys(banObjects.commonToponyms).length} toponymes et ${Object.keys(banObjects.addresses).length} adresses`);
+      logger.info(`[writer] Enregistrement MongoDB de BAL ${parsed.id} avec ${Object.keys(banObjects.districts).length} districts, ${Object.keys(banObjects.commonToponyms).length} toponymes et ${Object.keys(banObjects.addresses).length} adresses`);
       await Promise.all([
         // Injection des districts
         mongoCollectionDistricts.insertMany(
@@ -129,16 +131,16 @@ async function main() {
           })),
         ),
       ]);
-      console.log(`[writer] BAL ${parsed.id} enregistrée en MongoDB`);
+      logger.info(`[writer] BAL ${parsed.id} enregistrée en MongoDB`);
 
       ackOrNack();
     } catch (err) {
-      console.error('[writer] erreur de traitement message :', err);
+      logger.error('[writer] erreur de traitement message :', err);
       ackOrNack();
     }
   });
 
-  console.log('[writer] En écoute sur bal.ready...');
+  logger.log('[writer] En écoute sur bal.ready...');
 }
 
 main();
